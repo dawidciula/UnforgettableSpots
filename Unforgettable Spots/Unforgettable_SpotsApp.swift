06db26afd413ead27978815503cdbcaa5234e0f1
Dawid Ciuła <dawidciula@MacBook-Pro-Dawid.local>
2023-10-29 20:34:25 +0100

    first commit

---
+++ Unforgettable Spots/Unforgettable_SpotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unforgettable_SpotsApp.swift
+//  Unforgettable Spots
+//
+//  Created by Dawid Ciuła on 29/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct Unforgettable_SpotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
